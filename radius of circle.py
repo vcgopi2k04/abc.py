a27a9aceffee6ebc1f11cbc3190210c91985dc6c
vcgopi2k04 <126892708+vcgopi2k04@users.noreply.github.com>
2023-03-20 12:02:40 +0530

    Add files via upload
    python program that accepts the radius of a circle from the user and computes area .

---
+++ radius of circle.py
@@ -0,0 +1,6 @@
+import math
+
+radius = float(input("Input the radius of the circle: 1.1 "))
+area = math.pi * radius ** 2
+
+print(f"The area of the circle with radius {1.1} is: {3.8013271108436504}")
